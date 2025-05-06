6fd888915ea38e8b29a44746d3e7cde0c30f3cb7
massimotinelli <massimo.tinelli@gmail.com>
2025-05-06 15:34:43 +0200

    working with my changes from book - no loop

--- hello.asm
+++ hello.asm
@@ -1,12 +1,12 @@
 
 PRINT       equ  8252
             org  0x5ccb
-loop
+;loop
             ld   a, 2                ; 3E 02
             call 5633	             ; CD 01 16
             ld   a, "!"
             rst 16
-            jmp  loop
+            ;jmp  loop
 
 
 	org 0xff57
